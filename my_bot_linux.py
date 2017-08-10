6c2cc2e929371755874678d7bad0a7ac15719060
kwan <303198069@qq.com>
2017-08-10 22:53:19 +0800

    misc

---
+++ my_bot_linux.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+#
+
+from datetime import datetime
+from wxbot import *
+
+
+class MyWXBot(WXBot):
+    def schedule(self):
+        hour = datetime.now().hour
+        minute = datetime.now().minute
+        if 9 <= hour <= 23:
+            if minute % 10 == 0:
+                self.send_msg(u'欢乐今宵', u'OK~~~%s' %datetime.now())
+
+        time.sleep(10)
+
+
+def main():
+    bot = MyWXBot()
+    bot.DEBUG = True
+    bot.conf['qr'] = 'tty'
+    bot.run()
+
+
+if __name__ == '__main__':
+    print 'Program Starting @time:%s' % datetime.now()
+    main()
